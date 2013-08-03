d596b065f4dc4e662165d2f65ef00583b13f7950
kgbsmurf <andreykireev@gmail.com>
2013-08-03 19:40:26 -0400

    Updated Test + Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,16 +19,40 @@ describe "Static pages" do
   describe "Help page" do
 	  it "should have the content 'Luxury Builder Directory Help and Support'" do
 		  visit '/static_pages/help'
-		  page.should have_content('Luxury Builder Directory Help and Support')
+		  page.should have_selector('title',text: 'Luxury Builder Directory Help and Support')
 	  end
+	  
+	  it "should have a right title" do
+	    visit '/static_pages/home'
+	    page.should have_selector('title',
+				     text: "Help and Support | Luxury Builder Directory")
+    end
   end
 
   describe "About page" do
 	  it "should have the content 'About Luxury Builder Directory'" do
 		  visit '/static_pages/about'
-		  page.should have_content('About Luxury Builder Directory')
+		  page.should have_selector('h1', text: 'About Luxury Builder Directory')
 	  end
+	  
+	  it "should have a right title" do
+	    visit '/static_pages/home'
+	    page.should have_selector('title',
+				     text: "About | Luxury Builder Directory")
+    end
   end
 
+    describe "Contact page" do
+	  it "should have the content 'Contact Luxury Builder Directory'" do
+		  visit '/static_pages/about'
+		  page.should have_selector('h1', text: 'Contact Luxury Builder Directory')
+	  end
+	  
+	  it "should have a right title" do
+	    visit '/static_pages/home'
+	    page.should have_selector('title',
+				     text: "Contact | Luxury Builder Directory")
+    end
+  end
 
 end
